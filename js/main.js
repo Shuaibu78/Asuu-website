5462748fe9eb14deb49fcb0122fbe7cce9bd622b
shuaibu muhammad <devshuaib@gmail.com>
2021-02-09 10:19:13 +0100

    completed post page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -12,9 +12,9 @@ function currentSlide(n) {
 }
 
 // function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
 //   if (n > slides.length) { slideIndex = 1 }
 //   if (n < 1) { slideIndex = slides.length }
 //   for (i = 0; i < slides.length; i++) {
@@ -27,12 +27,12 @@ function currentSlide(n) {
 //   dots[slideIndex - 1].className += " active";
 // }
 
-var slideIndex = 0;
+let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -43,10 +43,33 @@ function showSlides() {
 }
 
 function myFunction() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
   }
 }
+
+
+// coming soon
+
+let countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+let x = setInterval(function() {
+
+  let now = new Date().getTime();
+
+  let distance = countDownDate - now;
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
