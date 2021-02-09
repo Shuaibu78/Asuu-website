3db16aa68d9e76e78f8a2d3cb732868f6b5a3d5d
shuaibu muhammad <devshuaib@gmail.com>
2021-02-09 10:40:30 +0100

    added about page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-let slideIndex = 1;
+
+var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -12,9 +13,9 @@ function currentSlide(n) {
 }
 
 // function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
 //   if (n > slides.length) { slideIndex = 1 }
 //   if (n < 1) { slideIndex = slides.length }
 //   for (i = 0; i < slides.length; i++) {
@@ -27,12 +28,12 @@ function currentSlide(n) {
 //   dots[slideIndex - 1].className += " active";
 // }
 
-let slideIndex = 0;
+var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -43,14 +44,13 @@ function showSlides() {
 }
 
 function myFunction() {
-  let x = document.getElementById("myTopnav");
+  var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
   }
 }
-
 
 // coming soon
 
